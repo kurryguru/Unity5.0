bdf8021bf49979b371928dfafbefd14c3efc86ea
kurryguru <pantji@yahoo.com>
2015-07-22 12:36:49 -0500

    New changes for Aniket to Read
    New changes for Aniket to Read

--- C# Tutorial/Projects/SecondProject/SecondProject/Program.cs
+++ C# Tutorial/Projects/SecondProject/SecondProject/Program.cs
@@ -9,6 +9,7 @@ namespace SecondProject
 			Console.WriteLine ("Hello World!");
 			Console.WriteLine ("I am a computer. I am here to do good things");
 			Console.WriteLine ("Aniket you will have to work hard to keep up");
+			Console.ReadKey ();
 		}
 	}
 }
