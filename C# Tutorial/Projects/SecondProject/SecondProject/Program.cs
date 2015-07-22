19f2efd6ba4bc8f6ddeaaa7b1f5f83f9c88426a0
kurryguru <pantji@yahoo.com>
2015-07-22 12:31:42 -0500

    Made some new changes.
    Changed the directory of second project as it was incorrect.

---
+++ C# Tutorial/Projects/SecondProject/SecondProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SecondProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("I am a computer. I am here to do good things");
+			Console.WriteLine ("Aniket you will have to work hard to keep up");
+		}
+	}
+}
