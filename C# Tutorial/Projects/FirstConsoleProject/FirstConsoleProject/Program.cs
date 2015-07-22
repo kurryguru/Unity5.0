aa965d7b7412df544bcaa1e69a2da92afeed369e
kurryguru <pantji@yahoo.com>
2015-07-22 12:14:43 -0500

    Added two new programs
    Added two new programs
    1) First Project
    2) Second Project

---
+++ C# Tutorial/Projects/FirstConsoleProject/FirstConsoleProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Himanshu and Aniket!");
+			Console.WriteLine ("This is a great start!");
+		}
+	}
+}
