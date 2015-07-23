fd604efa31a3a526c349cb3fdd105f674a0f4716
pant2002 <pant.aniket2002123@gmail.com>
2015-07-22 19:56:01 -0500

    Added Challenge #1
    Added Challenge #1 received from Chapter #1.

---
+++ C# Tutorial/Projects/Challenges/Challenge#1/Challenge#1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Challenge1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Age : 13");
+			Console.WriteLine ("Name : Aniket Pant");
+			Console.ReadKey ();
+			Console.WriteLine ("Red");
+			Console.ReadKey ();
+		}
+	}
+}
