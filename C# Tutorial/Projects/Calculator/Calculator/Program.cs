c414bb38a32f39c12a17eec0581d4e62d2b9ff77
pant2002 <pant.aniket2002123@gmail.com>
2015-07-26 21:42:27 -0500

    Changes to Calculator Program.cs
    Changes to Calculator Program.cs

---
+++ C# Tutorial/Projects/Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)	//Main method called when program starts.
+		{
+			Double num01;
+			Double num02;
+
+			Console.Write ("Type a number to be multiplied: ");	//Enter first number.
+			num01 = Convert.ToDouble (Console.ReadLine ());	//Enter first number.
+
+			Console.Write ("Type another number to be multiplied: ");	//Enter second number.
+			num02 = Convert.ToDouble (Console.ReadLine ()); //Enter second number.
+
+			Console.WriteLine ("The result is: " + num01 * num02); //Multiplication Result
+			Console.ReadKey ();	//Multiplication Result
+		}
+	}
+}
