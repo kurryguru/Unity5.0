31c4c581c269984f0ff1dac1a97c6ead79ac061d
pant2002 <pant.aniket2002123@gmail.com>
2015-07-26 23:22:32 -0500

    Challenge#3 Edits.
    Challenge#3 Edits

--- C# Tutorial/Projects/IfStatements/IfStatements/Program.cs
+++ C# Tutorial/Projects/IfStatements/IfStatements/Program.cs
@@ -6,7 +6,17 @@ namespace Calculator
 	{
 		public static void Main (string[] args)	//Main method called when program starts.
 		{
+			
 			Console.WriteLine ("How old are you?");
+			int age = Convert.ToInt32 (Console.ReadLine ());
+			if (age < 18) {
+				Console.WriteLine ("That's too bad! You will have to wait a couple of years.");
+			} else if (age == 18) {
+				Console.WriteLine ("Phew, barely made it!");
+			} else {
+				Console.WriteLine ("You are allowed to enter.");
+			}
+			Console.ReadKey ();
 		}
 	}
 }
