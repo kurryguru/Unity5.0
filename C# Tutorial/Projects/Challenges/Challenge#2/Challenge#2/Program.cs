255b14c45c0df948abc9f47cf8b864f529be1a33
pant2002 <pant.aniket2002123@gmail.com>
2015-07-26 21:41:57 -0500

    Changes to Calculator/Challenge#2
    Made changes to Calculator/Challenge#2 on July 26, 2015.

--- C# Tutorial/Projects/Challenges/Challenge#2/Challenge#2/Program.cs
+++ C# Tutorial/Projects/Challenges/Challenge#2/Challenge#2/Program.cs
@@ -6,7 +6,7 @@ namespace Challenge2
 	{
 		public static void Main (string[] args)
 		{
-			double num01;
+			Start:
 			double num02;
 
 			Console.Write ("Type a number to be divided: ");
@@ -15,6 +15,10 @@ namespace Challenge2
 			num02 = Convert.ToDouble (Console.ReadLine ());
 			Console.WriteLine (num01 + " divided by " + num02 + " is equal to: " + num01 / num02);
 			Console.ReadKey ();
+
+			Console.ReadKey ();
+			Console.WriteLine ();
+			goto Start; //Jumps back to start.
 		}
 	}
 }
---
+++ C# Tutorial/Projects/IfStatements/IfStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfStatements
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("How old are you?");
+		}
+	}
+}
